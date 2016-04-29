63e2f402cdb14075edfca806bd12fc9235911c80
Colin Standefer <cstandefer@alumni.berklee.edu>
2016-04-29 15:07:55 -0500

    Changed Index Title/h1, modified accompanying CSS & route

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Zombie Kit' });
 });
 
 // function isLoggedIn(req, res, next) {
